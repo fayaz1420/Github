8255efaaf0d77299ba3c5d03bd9dad1180d832f5
fayaz1420 <fayaz14@nmsu.edu>
2026-01-28 16:01:41 -0600

    Add source and pom only

---
+++ src/main/java/net/github/springboot/SpringbootGithubApplication.java
@@ -0,0 +1,13 @@
+package net.github.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGithubApplication.class, args);
+	}
+
+}
